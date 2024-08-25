f9c38f3d4f0f19dc7a1b37dc7f5c4f2871b1e508
MINGYI SU <125218610+MINGYISU@users.noreply.github.com>
2024-08-25 17:02:02 -0400

    Updated code style

--- src/network/static/network/js/flw_btn.js
+++ src/network/static/network/js/flw_btn.js
@@ -1,47 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
-            
-    if (document.querySelector("#flw_btn")) {
-        document.querySelector("#flw_btn").onclick = function() {
-            const IdolID = this.dataset.idolid;
-            const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-            const url = this.dataset.url;
-            fetch(url, {
-                method: "POST", 
-                headers: {
-                    "X-CSRFToken": csrftoken, 
-                    "Content-Type": "application/json"
-                }, 
-                body: JSON.stringify({
-                    "IdolID": IdolID
-                })
-            })
-            .catch(error => {
-                console.log("Error: ", error)
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('HTTP error! status: ${response.status}')
-                }
-                return response.json()
-            })
-            .then(data => {
-                console.log(data)
-                if (data.success) {
-                    if (data.flwed) {
-                        this.innerHTML = "Unfollow";
-                        document.querySelector("#n_flwers").innerHTML = data.n_flwer;
-                    } else {
-                        this.innerHTML = "Follow";
-                        document.querySelector("#n_flwers").innerHTML = data.n_flwer;
-                    }
-                } else {
-                    alert("Action Failed!");
-                }
-            })
-            .catch(error => {
-                console.log("Error: ", error)
-            })
-        }
-    }
-
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector("#flw_btn")) {
+    document.querySelector("#flw_btn").onclick = function () {
+      const IdolID = this.dataset.idolid;
+      const csrftoken = document.querySelector(
+        "[name=csrfmiddlewaretoken]"
+      ).value;
+      const url = this.dataset.url;
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "X-CSRFToken": csrftoken,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          IdolID: IdolID,
+        }),
+      })
+        .catch((error) => {
+          console.log("Error: ", error);
+        })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("HTTP error! status: ${response.status}");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          if (data.success) {
+            if (data.flwed) {
+              this.innerHTML = "Unfollow";
+              document.querySelector("#n_flwers").innerHTML = data.n_flwer;
+            } else {
+              this.innerHTML = "Follow";
+              document.querySelector("#n_flwers").innerHTML = data.n_flwer;
+            }
+          } else {
+            alert("Action Failed!");
+          }
+        })
+        .catch((error) => {
+          console.log("Error: ", error);
+        });
+    };
+  }
 });
--- src/network/static/network/js/like_btn.js
+++ src/network/static/network/js/like_btn.js
@@ -1,49 +1,48 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("#like_btn").forEach(function (button) {
+    button.onclick = function () {
+      const ID = this.dataset.postid;
+      const csrftoken = document.querySelector(
+        "[name=csrfmiddlewaretoken]"
+      ).value;
+      const url = this.dataset.url;
 
-    document.querySelectorAll("#like_btn").forEach(function(button) {
-        button.onclick = function() {
-            const ID = this.dataset.postid;
-            const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-            const url = this.dataset.url;
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "X-CSRFToken": csrftoken,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          postid: ID,
+        }),
+      })
+        .catch((error) => {
+          console.log("Error: ", error);
+        })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("HTTP error! status: ${response.status}");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          if (data.success) {
+            var num = document.querySelector("#n_like" + ID);
 
-            fetch(url, {
-                method: "POST", 
-                headers: {
-                    "X-CSRFToken": csrftoken, 
-                    "Content-Type": "application/json"
-                }, 
-                body: JSON.stringify({
-                    "postid": ID
-                })
-            })
-            .catch(error => {
-                console.log("Error: ", error)
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("HTTP error! status: ${response.status}")
-                }
-                return response.json()
-            })
-            .then(data => {
-                console.log(data)
-                if (data.success) {
-                    var num = document.querySelector("#n_like"+ID)
-                    
-                    if (data.liked) {
-                        num.innerHTML = "💔 " + data.n_liker
-                    } else {
-                        num.innerHTML = "❤️ " + data.n_liker
-                    }
-                } else {
-                    alert("Action Failed!");
-                }
-            })
-            .catch(error => {
-                console.log("Error: ", error)
-            })
-                    
-        }
-    });
-
+            if (data.liked) {
+              num.innerHTML = "💔 " + data.n_liker;
+            } else {
+              num.innerHTML = "❤️ " + data.n_liker;
+            }
+          } else {
+            alert("Action Failed!");
+          }
+        })
+        .catch((error) => {
+          console.log("Error: ", error);
+        });
+    };
+  });
 });
